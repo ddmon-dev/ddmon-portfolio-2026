ca12d0a4206529b8423fa5459ffa36eacc95bfbd
dd2mon <ldhman91@gmail.com>
2026-05-21 14:54:15 +0900

    renew: 프로젝트 초기화

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  distDir: process.env.IS_CLAUDE ? '.next-claude' : undefined,
 };
 
 export default nextConfig;
