c3a65accbd89c9017908ea36a10e44f926011cb4
ddmon-dev <ldhman91@gmail.com>
2026-07-16 01:26:21 +0900

    hotfix: robots.txt 전체 허용 (에이전트 접근 차단 해제, 색인 차단은 meta noindex 유지)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,20 +1,7 @@
 import type { MetadataRoute } from 'next';
 
-const socialCrawlers = [
-  'facebookexternalhit',
-  'Twitterbot',
-  'Slackbot-LinkExpanding',
-  'LinkedInBot',
-  'kakaotalk-scrap',
-  'Discordbot',
-  'TelegramBot',
-];
-
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      { userAgent: socialCrawlers, allow: '/' },
-      { userAgent: '*', disallow: '/' },
-    ],
+    rules: { userAgent: '*', allow: '/' },
   };
 }
